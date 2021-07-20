7eb8e58da7be2de0a715ca5fd697048aff05d2b1
Charan <srichu.kattamuru@gmail.com>
2021-07-21 02:06:44 +0530

    Refactor

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from utils import *
 
 grid_size = 1000
-side_length = 25
+side_length = 50
 
 grid_obj = Grid(grid_size, side_length)
 grid_obj.generate_grid()
 grid_obj.generate_points()
 
-
 for val in np.arange(1, 10, 0.5):
-    matrix = np.array([[val, 0],
-                       [0, 1]])
+    matrix = np.array([[1, 0],
+                       [val, 1]])
 
     _, new_pts = transform(grid_obj, matrix)
     grid = grid_obj.plot(new_pts)
     cv2.imshow("Grid", grid)
-    if cv2.waitKey(250) == 27:
+    if cv2.waitKey(100) == 27:
         break
 
 cv2.destroyAllWindows()
